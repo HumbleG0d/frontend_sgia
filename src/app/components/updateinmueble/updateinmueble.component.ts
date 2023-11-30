7e2d4e8ae7eb42f1a388d1053e2d7906a5f33203
HumbleG0d <sergiouni2000@gmail.com>
2023-11-30 13:13:51 -0500

    Add logical for component updateinmueble

--- src/app/components/updateinmueble/updateinmueble.component.ts
+++ src/app/components/updateinmueble/updateinmueble.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
+import { Inmueble } from 'src/app/model/Inmueble';
 
 @Component({
   selector: 'app-updateinmueble',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./updateinmueble.component.css']
 })
 export class UpdateinmuebleComponent {
+  inm?: Inmueble
+  data: any
 
+
+  constructor(private service: AppService, private route: ActivatedRoute, private router : Router) { }
+
+  ngOnInit(): void {
+    let id = this.route.snapshot.params['id'];
+    this.service.getUserById(id).subscribe(data => {
+      this.inm = data
+      console.log(this.inm)
+    })
+  }
+
+  form = new FormGroup({
+    inmueble: new FormControl('', [Validators.required]),
+    direccion: new FormControl('', [Validators.required]),
+    id_inmueble: new FormControl('', [Validators.required])
+  })
+
+  submit(){
+    this.data = this.form.value
+    console.log(this.data)
+    
+    this.service.updateInmueble(this.inm?.id_inmueble, this.data).subscribe(data => {
+      console.log(data)
+    })
+
+    this.router.navigate(['/']);
+  }
 }
