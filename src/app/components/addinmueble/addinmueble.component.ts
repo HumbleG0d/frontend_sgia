fcd53b5a60931a96c405018cfb981a209b829038
HumbleG0d <sergiouni2000@gmail.com>
2023-11-30 13:11:01 -0500

    Add logical for component addinmueble

--- src/app/components/addinmueble/addinmueble.component.ts
+++ src/app/components/addinmueble/addinmueble.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AppService } from 'src/app/app.service';
 
 @Component({
   selector: 'app-addinmueble',
@@ -6,5 +9,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./addinmueble.component.css']
 })
 export class AddinmuebleComponent {
+  constructor(private service: AppService, private router: Router) { }
+  data: any
+  
+  form = new FormGroup({
+    inmuebles: new FormControl('', [Validators.required]),
+    direccion: new FormControl('', [Validators.required]),
+    id_inmueble: new FormControl('', [Validators.required])
+  })
+  ngOnInit(): void {
+  }
+  submit(){
+    this.data = this.form.value
+    console.log(this.data)
 
+    this.service.addinmueble(this.data).subscribe(data => {
+      console.log(data)
+    })
+    this.router.navigate(['/']);
+  }
 }
+
