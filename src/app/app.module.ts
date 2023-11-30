bbfc1e583ddefc820740192c8c38f404d51a3bff
HumbleG0d <sergiouni2000@gmail.com>
2023-11-30 13:16:50 -0500

    Add declaratons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
+import { ListinmuebleComponent } from './components/listinmueble/listinmueble.component';
+import { AddinmuebleComponent } from './components/addinmueble/addinmueble.component';
+import { UpdateinmuebleComponent } from './components/updateinmueble/updateinmueble.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddinmuebleComponent,
+    UpdateinmuebleComponent,
+    ListinmuebleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
