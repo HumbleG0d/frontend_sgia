9213c019e0c7659257e9af46ee75d4f22fa6057c
HumbleG0d <sergiouni2000@gmail.com>
2023-12-03 09:54:46 -0500

    Add path update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListinmuebleComponent } from './components/listinmueble/listinmueble.component';
 import { AddinmuebleComponent } from './components/addinmueble/addinmueble.component';
+import { UpdateinmuebleComponent } from './components/updateinmueble/updateinmueble.component';
 
 const routes: Routes = [
   { path: '', component: ListinmuebleComponent },
-  { path: 'add', component: AddinmuebleComponent }
+  { path: 'add', component: AddinmuebleComponent },
+  {path : 'update/:id' , component: UpdateinmuebleComponent}
 ];
 
 @NgModule({
